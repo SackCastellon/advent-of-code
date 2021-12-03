5bd9fe671de446a87fadd4bbe183780a5257c1db
Juan José González-Abril <juanjo@gonzabril.es>
2021-12-03 18:46:39 +0100

    Refactor 2021 day 1 puzzle

--- src/main/kotlin/me/sackcastellon/aoc/y2021/Day01.kt
+++ src/main/kotlin/me/sackcastellon/aoc/y2021/Day01.kt
@@ -2,19 +2,16 @@ package me.sackcastellon.aoc.y2021
 
 import me.sackcastellon.aoc.Puzzle
 import java.nio.file.Path
-import kotlin.io.path.useLines
+import kotlin.io.path.readLines
 
 object Day01 : Puzzle<Int> {
-    override fun solve1(input: Path): Int = input.useLines { lines ->
-        lines.map { it.toInt() }
-            .zipWithNext()
-            .count { (a, b) -> b > a }
-    }
+    override fun solve1(input: Path): Int = input.readLines()
+        .map(String::toInt)
+        .zipWithNext()
+        .count { (a, b) -> b > a }
 
-    override fun solve2(input: Path): Int = input.useLines { lines ->
-        lines.map { it.toInt() }
-            .windowed(size = 3) { it.sum() }
-            .zipWithNext()
-            .count { (a, b) -> b > a }
-    }
+    override fun solve2(input: Path): Int = input.readLines()
+        .map(String::toInt)
+        .windowed(size = 4)
+        .count { (a, _, _, b) -> b > a }
 }
